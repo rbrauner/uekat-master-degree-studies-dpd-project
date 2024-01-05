bb8a16e2704d5a035610c0138e33aaf4a6ddb116
rbrauner <rafalbrauner@gmail.com>
2024-01-05 19:02:57 +0100

    feat: reload

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from src.app import app
 
 if __name__ == '__main__':
     load_dotenv()
-    uvicorn.run(app, host=os.getenv('APP_HOST', '0.0.0.0'), port=int(os.getenv('APP_PORT', 8000)))
+    uvicorn.run(app, host=os.getenv('APP_HOST', '0.0.0.0'), port=int(os.getenv('APP_PORT', 8000)), reload=True)
