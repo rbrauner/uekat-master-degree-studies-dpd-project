c9c1c1081d1beeef082b59911c9daaf70e813ef2
rbrauner <rafalbrauner@gmail.com>
2024-01-05 17:53:54 +0100

    feat: .env

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+from dotenv import load_dotenv
 import uvicorn
-
 from src.app import app
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    load_dotenv()
+    uvicorn.run(app, host=os.getenv('APP_HOST', '0.0.0.0'), port=int(os.getenv('APP_PORT', 8000)))
