026080213a0fd32dac4627fce5973a1a12f44a6a
oreganoli <3611916+oreganoli@users.noreply.github.com>
2024-01-14 10:02:15 +0100

    apply code review suggestions

--- src/routes/cities_around/cities_around.py
+++ src/routes/cities_around/cities_around.py
@@ -4,10 +4,11 @@ from src.services.cities_around_service import CitiesAroundService
 cities_around_router = APIRouter()
 cities_around_service = CitiesAroundService()
 
+
 @cities_around_router.get("/cities-around")
-async def cities_around(c: str, d: float):
+async def cities_around(city: str, distance_km: float):
     try:
-        cities_around = cities_around_service.get_cities_around(c, d)
-        return {"city": c, "distance": d, "cities_around": cities_around}
+        cities_around = cities_around_service.get_cities_around(city, distance_km)
+        return {"city": city, "distance": distance_km, "cities_around": cities_around}
     except Exception as e:
         return {"error": str(e)}
--- src/services/cities_around_service.py
+++ src/services/cities_around_service.py
@@ -1,12 +1,13 @@
 import requests
 
+
 class CitiesAroundService:
     def __init__(self):
         pass
 
-    def get_cities_around(self, city_name: str, distance: float):
+    def get_cities_around(self, city_name: str, distance_km: float):
         coordinates = self.get_coordinates(city_name)
-        cities_around = self.get_cities_around_coordinates(coordinates, distance)
+        cities_around = self.get_cities_around_coordinates(coordinates, distance_km)
         return cities_around
 
     def get_coordinates(self, city_name: str):
@@ -21,13 +22,12 @@ class CitiesAroundService:
         longitude = float(data[0]["lon"])
         return {"lat": latitude, "lon": longitude}
 
-    def get_cities_around_coordinates(self, coordinates: tuple, distance: float):
-        distance = distance * 1000
-        url = f"https://overpass-api.de/api/interpreter?data=[out:json];(node(around:{distance},{coordinates['lat']},{coordinates['lon']})[place~\"city|town\"];rel(around:{distance},{coordinates['lat']},{coordinates['lon']})[place~\"city|town\"];);out;"
+    def get_cities_around_coordinates(self, coordinates: tuple, distance_km: float):
+        distance_km = distance_km * 1000
+        url = f"https://overpass-api.de/api/interpreter?data=[out:json];(node(around:{distance_km},{coordinates['lat']},{coordinates['lon']})[place~\"city|town\"];rel(around:{distance_km},{coordinates['lat']},{coordinates['lon']})[place~\"city|town\"];);out;"
         response = requests.get(url)
         data = response.json()
         cities_around = []
         for city in data["elements"]:
             cities_around.append(city["tags"]["name"])
         return cities_around
-
